650360a854f3de7f84e66a11ba0f1fc083919daf
Michael <mikeolams@yahoo.com>
2018-09-14 09:33:58 +0100

    submit11

--- js/app.js
+++ js/app.js
@@ -7,39 +7,22 @@ const cardDisplayBoard = document.querySelector('.deck'),
 let countDisplay = document.querySelector('.moves');
 let textDisplay = document.querySelector('.text');
 let cardsShuffled = shuffle(cardsArray);
-
-let startBtn=document.querySelector('.start');
-
+let startBtn = document.querySelector('.start');
 startBtn.addEventListener('click', gameCommence, false);
 
-// var tickCount = 0;
+let on = 0;
 
-let on=0;
-// if (minuteCount>=0 && tickCount>0){on =1; console.log(tickCount)}
 // calling of new card function
-function gameCommence(){swal("Please type your name here:", {
-  content: "input",
-})
-.then((value) => {
-  swal(`You are identify as: ${value}`);
-});
-on =1;
-// while (matchedCards.length<16) {gameTimer(event);}
-clock();
-// if (minuteCount>=0 && tickCount>0){on =1; console.log(tickCount)}
+function gameCommence() {
+	swal("Please type your name here:", {
+		content: "input",
+	}).then((value) => {
+		swal(`You are identify as: ${value}`);
+	});
+	on = 1;
+	clock();
 }
 
-// function openNav() {
-//   cardDisplayBoard.style.width = "660px";
-// }
-
-// function closeNav() {
-//   cardDisplayBoard.style.width = "0";}
-
-
-// newCards();
-// u = shuffle(cardsToPlay);
-// I have two issues, line 212 (setTimeout)and  below is the other one this is not iterating as intended. at it is my programme wont run because of the issue but if i comment out this for loop. it will run but no reshuffle is done.
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -63,82 +46,57 @@ function shuffle(array) {
 }
 let count = 0;
 let currentTarget = document.querySelector('.deck');
-
- rating();
+rating();
 // function defined to attached the shuffled cards to the board in order to make it ready for a new game
 function newGame() {
-	if(matchedCards.length >=2){for (let i = 0; i <= cardsShuffled.length; i++) {
-		cardDisplayBoard.appendChild(cardsShuffled[i]);
+	if (matchedCards.length >= 2) {
+		for (let i = 0; i <= cardsShuffled.length; i++) {
+			cardDisplayBoard.appendChild(cardsShuffled[i]);
+		}
 	}
 }
-}
 
-// currentTarget.addEventListener('click', gameTimer, false);
-
+// adding listerner to cards.
 currentTarget.addEventListener('click', showSymbol, false);
+
 //function defined to show any clicked card on the board
 function showSymbol(event) {
 	if (event.target !== event.currentTarget) {
-		event.preventDefault();
-		// const clickTarget = event.target;
-		// console.log(clickTarget);	
+		event.preventDefault();	
 		if (event.target.className === "card") {
-			if(on ==1){
-			event.target.classList.add('show');
-			openCard(event);
+			if (on == 1) {
+				event.target.classList.add('show');
+				openCard(event);
 			}
 		}
 	}
-	// open(event);
 }
 
-
+//defining some parameters for the open and match cards
 let openedCards = [],
 	matchedCards = [];
-// 	origin = currentTarget;
-// console.log(origin);
+
 //function defined to open any clicked card on the board
 function openCard(event) {
 	if (event.target !== event.currentTarget) {
-		event.preventDefault();
-		// const clickTarget = event.target;
-		// const clickTarget1 = event.target.className
-		// console.log(clickTarget,clickTarget1 );	
+		event.preventDefault();	
 		if (event.target.className === "card show") {
 			event.target.classList.add('open');
 			openedCards.push(event.target);
-			cardMatchCheck(event);
-			// console.log(openedCards );	
+			cardMatchCheck(event);	
 		}
 	}
 }
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
 //function defined to check if two card opened matches or not
 function cardMatchCheck(event) {
 	if (event.target !== event.currentTarget) {
-		event.preventDefault();
-		// const clickTarget = event.target;
-		// const clickTarget1 = event.target.className
-		// console.log(clickTarget, clickTarget1);
-		// console.log( openedCards );	
+		event.preventDefault();	
 		if (openedCards.length > 1) {
-			// console.log( openedCards );
 			if (event.target.className === "card show open") {
-				// console.log( event.target.className );
 				if (event.target.firstElementChild.className === openedCards[0].firstElementChild.className) {
 					event.target.classList.add('match');
 					openedCards[0].classList.add('match');
-					// console.log(event.target.firstElementChild.className);
 					event.target.classList.remove('show');
 					event.target.classList.remove('open');
 					openedCards[0].classList.remove('show');
@@ -146,7 +104,6 @@ function cardMatchCheck(event) {
 					matchedCards.push(openedCards[1], openedCards[0]);
 					openedCards = [];
 					gameComplete(event);
-					// openedCards.push(event.target)
 				} else {
 					unmatchCardCheck(event)
 				}
@@ -155,105 +112,69 @@ function cardMatchCheck(event) {
 		}
 	}
 }
+
 //function defined to restore opened cards to normal if they are not match
 function unmatchCardCheck(event) {
 	if (event.target !== event.currentTarget) {
 		event.preventDefault();
 		if (event.target.firstElementChild.className !== openedCards[0].firstElementChild.className) {
-			// console.log(openedCards);
-			// console.log(matchedCards);
-			setTimeout(function () {
-        event.target.classList.remove('show');
-			event.target.classList.remove('open');
-			openedCards[0].classList.remove('show');
-			openedCards[0].classList.remove('open');
-			openedCards = [];
-    },600,"event");
-			// openedCards.pop(openedCards[1], openedCards[0]);
+			setTimeout(function() {
+				event.target.classList.remove('show');
+				event.target.classList.remove('open');
+				openedCards[0].classList.remove('show');
+				openedCards[0].classList.remove('open');
+				openedCards = [];
+			}, 600, "event");
 			console.log(openedCards);
 		}
 	}
 }
 
-// function unmatchCardCheck(event) {
-// 	if (event.target !== event.currentTarget) {
-// 		event.preventDefault();
-// 		if (event.target.firstElementChild.className !== openedCards[0].firstElementChild.className) {
-// 			// console.log(openedCards);
-// 			// console.log(matchedCards);
-// 			event.target.classList.remove('show');
-// 			event.target.classList.remove('open');
-// 			openedCards[0].classList.remove('show');
-// 			openedCards[0].classList.remove('open');
-// 			openedCards = [];
-// 			// openedCards.pop(openedCards[1], openedCards[0]);
-// 			console.log(openedCards);
-// 		}
-// 	}
-// }
 
+// // setTimeout(unmatchCardCheck, 100, "event");
+// setInterval(unmatchCardCheck, 1000, "event");
 
-// setTimeout(unmatchCardCheck, 1000, 'event');
-//the second issue is here, the timing is not functionin as I intend. This suppose to delay the flip but it is not.
-// setTimeout(function() {
-// 	unmatchCardCheck;
-// }, 1000, "event");
-// setTimeout(unmatchCardCheck, 100, "event");
-setInterval(unmatchCardCheck, 1000, "event");
 //function defined to operate the score board
 function moveCounter(event) {
 	if (event.target !== event.currentTarget) {
 		event.preventDefault();
-		// let textDisplay = document.querySelector('.text');
-		// console.log(count);
 		count += 1;
-		// console.log(count);
 		if (count < 2) {
 			textDisplay.innerText = " Move";
-			// console.log(textDisplay);
 		} else {
 			textDisplay.innerText = " Moves";
 		}
 		countDisplay.innerText = count;
-		// console.log(count);
 	}
 }
-// timer = ()=> {
-//         t = setTimeout(add, 1000);  
-// }
 
-// var count=document.getElementById('SecondsInput').value;
-// var counter=setInterval(gameTimer, 1000, "event"); //run it every 1 second
-// var tickCount = 0,
-// minuteCount = 0;
+//setting some parameters for the timer
+var counter, tickCount = 0,
+	minuteCount = 0;
 
-var counter, tickCount = 0, minuteCount = 0;
-
-function clock(){
-	var counter=setInterval(gameTimer, 1000, "event"); //run it every 1 second
-var tickCount = 0,
-minuteCount = 0;
+function clock() {
+	var counter = setInterval(gameTimer, 1000, "event"); //run it every 1 second
+	var tickCount = 0,
+		minuteCount = 0;
 }
-
 
 //function defined to tell the user game is completed
 function gameComplete(event) {
 	if (event.target !== event.currentTarget) {
 		event.preventDefault();
-		// console.log(origin);
-		// rating();
+		
 		if (matchedCards.length === 16) {
-			// console.log(origin);
+			// modal implentation by sweet swal
 			swal({
 				title: "Good job! Congratulations!!!",
 				text: "You matched all the cards! \n by completed the game after " + count + " moves \n Well done!!!",
 				icon: "success",
 			});
 			resetGame(event)
-			
 		}
 	}
 }
+
 //function defined to generate new set of cards ready to be played
 function newCards() {
 	for (item of cards) {
@@ -262,126 +183,114 @@ function newCards() {
 		cards = item;
 	};
 }
-//reset function called
+
+
+//reset function called and parameters set
 const resetButton = document.querySelector('.restart');
 const allCards = currentTarget.children;
+
 resetButton.addEventListener('click', resetGame, false);
 newElement = document.createElement('li');
 
 //reset function
 function resetGame(event) {
 	event.preventDefault();
-	if(on ==1){
+	if (on == 1) {
 		warning();
-	for (var i = 0; i < allCards.length; i++) {
-		allCards[i].classList.remove('show', 'open', 'match');
-		openedCards = [],
-			matchedCards = [];
+		for (var i = 0; i < allCards.length; i++) {
+			allCards[i].classList.remove('show', 'open', 'match');
+			openedCards = [],
+				matchedCards = [];
 			minuteCount = 0;
 			tickCount = 0;
-		count = -1;
-		moveCounter(event);
-		// shuffle(cardsCover);
-		newCards();
-		// console.log(origin);
-		newGame();
-		// console.log(origin);
-		restoreRating();
+			count = -1;
+			moveCounter(event);
+			newCards();
+			newGame();
+			restoreRating();
+		}
 	}
 }
-}
 
-function warning(){
+// reset function call this warning before execution
+function warning() {
 	swal({
-  title: "Are you sure you want to reset this game?",
-  text: "Once reset, you will have a new board to start a new game!",
-  icon: "warning",
-  buttons: true,
-  dangerMode: true,
-})
-.then((willDelete) => {
-  if (willDelete) {
-    swal("Thanks! Your current game is reset!", {
-      icon: "success",
-    });
-  } else {
-    swal("Your current game is not reset, you can continue!");
-  }
-});
-
+		title: "Are you sure you want to reset this game?",
+		text: "Once reset, you will have a new board to start a new game!",
+		icon: "warning",
+		buttons: true,
+		dangerMode: true,
+	}).then((willDelete) => {
+		if (willDelete) {
+			swal("Thanks! Your current game is reset!", {
+				icon: "success",
+			});
+		} else {
+			swal("Your current game is not reset, you can continue!");
+		}
+	});
 }
-
-
 
 //function defined to tell the user game is completed
-function gameTimer(){
-// event.preventDefault();
-  tickCount+=1;
+function gameTimer() {
+	tickCount += 1;
+	if (tickCount == 60) {
+		minuteCount += 1,
+			tickCount = 0;
+	}
+	if (minuteCount === 59) {
+		clearInterval(counter);
+		window.alert("Time Up!");
+		document.querySelector('.time').innerText = "Time Up!"
+	} else if (matchedCards.length === 16) {
+		window.alert("Well done!");
+		newCards();
+		matchedCards = 0;
+		tickCount = 0;
+		minuteCount = 0;
+	} else {
+		tickCount;
+	}
 
-  if (tickCount==60){minuteCount +=1,
-  	tickCount = 0;
-  }  
-
-  if (minuteCount === 59){
-     clearInterval(counter);
-     window.alert("Time Up!");
-     document.querySelector('.time').innerText = "Time Up!"
-  }else if (matchedCards.length === 16){
-  	window.alert("Well done!");
-  	// resetGame(event);
-  	newCards();
-  	// newCards(); it should be resetCards
-  	// newGame();
-  	matchedCards = 0;
-  	tickCount =0;
-  	minuteCount =0;
-  }else{tickCount;}
-
-  //Do code for showing the number of seconds here
-  document.querySelector('.time').innerText = minuteCount +" minutes "+ tickCount + " seconds"
-rating();
+	//Do code for showing the number of seconds here
+	document.querySelector('.time').innerText = minuteCount + " minutes " + tickCount + " seconds"
+	rating();
 }
 
-
-
+//setting rating parameters
 let ratingStart = document.querySelector('.stars').children;
-	let noStarMessage = document.querySelector('.stars');
+let noStarMessage = document.querySelector('.stars');
 
-function rating(){
-	
-	// let ratingStart = document.querySelector('.stars').children;
-	// let noStarMessage = document.querySelector('.stars');
-
-	if (countDisplay.innerText >14){ ratingStart[2].style.display ="none";console.log(count );
-
-	} 
-	// count;
-		// u[1].style.display =""
-		// console.log(count );
-
-	if(countDisplay.innerText >22){ ratingStart[2].style.display ="none";
-			ratingStart[1].style.display ="none";console.log(countDisplay.innerText, minuteCount );
-
-	} 
-	if(countDisplay.innerText >25){ 
-
-	  ratingStart[2].style.display ="none",
-			ratingStart[1].style.display ="none",
-			ratingStart[0].style.display ="none";
-			// newElement = document.createElement('li')
-			newElement.innerText = "You may keep trying but you\'ve got no star left after "+countDisplay.innerText+"moves"; 
-			noStarMessage.appendChild(newElement); console.log(minuteCount );
+//rating function to call for rating
+function rating() {
+	if (countDisplay.innerText > 14) {
+		ratingStart[2].style.display = "none";
+		console.log(count);
+	}
+	if (countDisplay.innerText > 22) {
+		ratingStart[2].style.display = "none";
+		ratingStart[1].style.display = "none";
+		console.log(countDisplay.innerText, minuteCount);
+	}
+	if (countDisplay.innerText > 25) {
+		ratingStart[2].style.display = "none",
+			ratingStart[1].style.display = "none",
+			ratingStart[0].style.display = "none";
+		newElement.innerText = "You may keep trying but you\'ve got no star left after " + countDisplay.innerText + "moves";
+		noStarMessage.appendChild(newElement);
+		console.log(minuteCount);
+	}
 }
 
+//function to restore default rating set
+function restoreRating() {
+	if (noStarMessage.childElementCount == 4) {
+		noStarMessage.removeChild(newElement);
+	}
+	for (item of ratingStart) {
+		item.style.display = "";
+	}
 }
-
-function restoreRating(){
-	if (noStarMessage.childElementCount == 4){noStarMessage.removeChild(newElement);}
-	for (item of ratingStart){ item.style.display ="";}
-
-}
-
-
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
